b8a682c5b2ec766c0407b42145f905425a69c83c
Allen Zou <allenzou@bu.edu>
2022-03-16 15:09:30 -0400

    added response parsing on send functions

--- faas-cli-scripts/sendFunctions.py
+++ faas-cli-scripts/sendFunctions.py
@@ -1,14 +1,13 @@
 import os
 import json
 import base64
+import subprocess
 
 os.system("faas-cli store deploy figlet")
 
-with open("shutdown.py", "r") as f:
+with open("print.py", "r") as f:
     src_code = f.read()
     src_code = json.dumps(src_code)
-
-print("SRCCODE", src_code)
 
 payload = {
     "\"fid\"": "\"test\"",
@@ -17,10 +16,23 @@ payload = {
     "\"lang\"": "\"micropython\""
 }
 
-# print("HERE",payload)
+
 payload_dumps = (json.dumps(payload))
-print(payload_dumps + "\n")
-os.system(f"echo {payload_dumps} | faas-cli invoke figlet")
+
+# Call the payload using faas-cli invoke and send to provider
+res = subprocess.check_output(f"echo {payload_dumps} | faas-cli invoke figlet", shell=True)
+
+# Take the JSON string of result and transform it into a JSON object and index the response body
+response = json.loads(json.loads(res)["ResponseBody"])
+
+# Check for error in response object
+try:
+    err = response["error"] 
+    if err:
+        print("ERROR HERE \n")
+except KeyError:
+    # Isolate the result from res to find the output of faas-cli invoke
+    result = response["result"]
+    print("RESULT IS: " + result + "\n")
 
 
-# print(json.loads(dumpy)["src"])
